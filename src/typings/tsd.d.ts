0a594bd73a83a4049ee85c051dd79e972515a181
aereal <aereal@aereal.org>
2015-07-18 01:15:23 +0900

    tsd reinstall

--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -1,4 +1,3 @@
-
-/// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="chrome/chrome.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
 /// <reference path="webrtc/MediaStream.d.ts" />
